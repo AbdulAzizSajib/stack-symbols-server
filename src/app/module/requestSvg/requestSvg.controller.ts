6f15f91478f2877307d14e078a0658b58dc9b28e
Abdul Aziz Sajib <abdulazizsajib@gmail.com>
2026-04-17 22:53:58 +0600

    Updated

--- src/app/module/requestSvg/requestSvg.controller.ts
+++ src/app/module/requestSvg/requestSvg.controller.ts
@@ -9,9 +9,13 @@ import geoip from "geoip-lite";
 const createRequest = catchAsync(async (req: Request, res: Response) => {
   const payload = req.body;
 
-  const ip = req.ip;
-
-  const geo = geoip.lookup(ip);
+// const ip = req.ip || "0.0.0.0";
+const ip =
+  (req.headers["x-forwarded-for"] as string)?.split(",")[0] ||
+  req.socket.remoteAddress ||
+  req.ip ||
+  "0.0.0.0";
+const geo = geoip.lookup(ip);
   const country = geo?.country || "Unknown";
 
   const result = await requestSvgService.createRequest(payload, {
